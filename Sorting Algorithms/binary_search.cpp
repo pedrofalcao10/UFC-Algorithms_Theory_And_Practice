1ede9c59f408ec52bcd36429a707a0e396bb9c81
Pedro Falcão <falcao.comp@gmail.com>
2024-04-19 13:20:30 -0300

    including recursive binary search

--- Sorting Algorithms/binary_search.cpp
+++ Sorting Algorithms/binary_search.cpp
@@ -11,3 +11,23 @@ int BinarySearch(int* vetor, int elemento, int start, int end){
 
     return -1; // If the target is not found, return -1
 }
+
+// Recursive binary search function
+int BinarySearchRec(int* vetor, int elemento, int start, int end) {
+    // Base case: if the start index exceeds the end index, the element is not found
+    if (start > end) return -1;
+
+    // Calculate the middle index of the current segment
+    int pivot = (start + end) / 2;
+
+    // If the target element is less than the element at the middle index,
+    // recursively search in the left sub-array
+    if (elemento < vetor[pivot]) return BinarySearchRec(vetor, elemento, start, pivot - 1);
+
+    // If the target element is greater than the element at the middle index,
+    // recursively search in the right sub-array
+    if (elemento > vetor[pivot]) return BinarySearchRec(vetor, elemento, pivot + 1, end);
+
+    // If the element at the middle index is equal to the target element, return the index
+    return pivot;
+}
