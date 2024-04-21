3f5a7677e31e27119b0f9357034014ce418a92de
Pedro Falcão <falcao.comp@gmail.com>
2024-04-21 13:08:54 -0300

    update LCS DP with tracking and including main()

--- Dinamic Programming/longest_common_subsequence.cpp
+++ Dinamic Programming/longest_common_subsequence.cpp
@@ -36,7 +36,7 @@ int LCS_rec_memo(string x, string y, int i, int j, int**& lcs) {
 }
 
 int LCS_DP(string x, string y) { // Bottom-up, non recursive
-    int i, j, m = x.size(), n = y.size();
+    int m = x.size(), n = y.size();
 
     int** lcs = new int*[m+1];
     for(int i = 0; i <= m; i++) lcs[i] = new int[n+1];
@@ -50,4 +50,121 @@ int LCS_DP(string x, string y) { // Bottom-up, non recursive
             else return max(lcs[i - 1][j], lcs[i][j - 1]);
 
     return lcs[m][n];
+}
+
+unsigned int LCS(string x, string y, char** &R) {
+    int m = x.size(), n = y.size();
+
+    int** lcs = new int*[m+1];
+    for (int i = 0; i <= m; i++) lcs[i] = new int[n+1];
+
+    if(R == NULL){
+        R = new char*[m+1];
+        for(int i = 0; i <= m; i++) R[i] = new char[n+1];
+    }
+
+    for (int i = 0; i <= m; i++) lcs[i][0] = 0;
+    for (int j = 0; j <= n; j++) lcs[0][j] = 0;
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (x[i-1] == y[j-1]) // (diagonal)
+            {
+                lcs[i][j] = lcs[i-1][j-1] + 1;
+                R[i][j] = '\\';
+            }
+            else if (lcs[i-1][j] == lcs[i][j-1]) // (up or left)
+            {
+                lcs[i][j] = lcs[i-1][j];
+                R[i][j] = '+';
+            }
+            else if (lcs[i-1][j] < lcs[i][j-1]) // (left)
+            {
+                lcs[i][j] = lcs[i][j-1];
+                R[i][j] = '-';
+            }
+            else // (up)
+            {
+                lcs[i][j] = lcs[i-1][j];
+                R[i][j] = '|';
+            }
+        }
+    }
+
+    int r = lcs[m][n];
+    for (int i = 0; i <= m; i++) delete[] lcs[i];
+    delete[] lcs;
+
+    return r;
+}
+
+void print_LCS(int i, int j, string x, char** R, string lcs) {
+    if ((i==0)||(j==0)) {
+        cout << lcs << "\n";
+        return;
+    }
+
+    if (R[i][j]=='\\') 
+    {
+        lcs = string(1,x[i-1]) + lcs;
+        print_LCS(i-1,j-1, x, R, lcs);
+    }
+    else if (R[i][j]=='+')
+    {
+        print_LCS(i-1,j, x, R, lcs);
+        print_LCS(i,j-1, x, R, lcs);
+    }
+    else if (R[i][j]=='-')
+    {
+        print_LCS(i,j-1, x, R, lcs);
+    }
+    else
+    {
+        print_LCS(i-1,j, x, R, lcs);
+    }
+}
+
+void print_matrix(string x, string y, char** R) {
+    int m = x.size(), n = y.size();
+
+    cout << "  " << y << "\n ";
+
+    for (int j = 0; j <= n; j++) cout << " ";
+
+    cout << "\n";
+
+    for (int i = 1; i <= m; i++) {
+        cout << x[i-1] << " ";
+        for (int j = 1; j <= n; j++) cout << R[i][j];
+        cout << "\n";
+    }
+}
+
+int main() {
+    string x = "ABCBDAB";
+    string y = "BDCABA";
+    cout << "Entrance X = " << x << "\n";
+    cout << "Entrance Y = " << y << "\n\n";
+
+    char** R = NULL;
+    cout << "Length of LCS = " << LCS(x, y, R);
+    cout << "\n\n";
+
+    string lcs;
+    cout << "All possible LCS's :\n";
+    print_LCS(x.size(), y.size(), x, R, lcs);
+    cout << "\n\n\n";
+
+    print_matrix(x,y,R);
+    if (R != NULL) {
+        for (int i = 0; i <= x.size(); i++) delete[] R[i];
+        delete[] R;
+    }
+
+    cout << "\n";
+    cout << "Simbol \\ (diagonal)\n";
+    cout << "Simbol | (up)\n";
+    cout << "Simbol - (left)\n";
+    cout << "Simbol + (up or left)\n\n";
+    return 0;
 }
