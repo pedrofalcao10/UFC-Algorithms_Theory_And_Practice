3fa1a9886d09df6a8d99bc998b0f7097e8b65a72
Pedro Falcão <falcao.comp@gmail.com>
2024-04-21 12:16:59 -0300

    update lcs DP and debug

--- Dinamic Programming/longest_common_subsequence.cpp
+++ Dinamic Programming/longest_common_subsequence.cpp
@@ -29,8 +29,25 @@ int LCS_memo(string x, string y, int** &lcs) {
 int LCS_rec_memo(string x, string y, int i, int j, int**& lcs) {
     if (lcs[i][j] >= 0) return lcs[i][j];
 
-    if (x[i] == y[j]) lcs[i][j] = LCS_rec_memo(x, y, i - 1, j - 1, lcs);
+    if (x[i] == y[j]) lcs[i][j] = LCS_rec_memo(x, y, i - 1, j - 1, lcs) + 1;
     else return max(LCS_rec_memo(x, y, i - 1, j, lcs), LCS_rec_memo(x, y, i, j - 1, lcs));
 
     return lcs[i][j];
+}
+
+int LCS_DP(string x, string y) { // Bottom-up, non recursive
+    int i, j, m = x.size(), n = y.size();
+
+    int** lcs = new int*[m+1];
+    for(int i = 0; i <= m; i++) lcs[i] = new int[n+1];
+
+    for (int i = 0; i <= m; i++) lcs[i][0] = 0;
+    for (int j = 0; j <= n; j++) lcs[0][j] = 0;
+    
+    for (int i = 1; i <= m; i++) 
+        for (int j = 1; j <= n; j++)
+            if (x[i] == y[j]) lcs[i][j] = lcs[i - 1][j - 1] + 1;
+            else return max(lcs[i - 1][j], lcs[i][j - 1]);
+
+    return lcs[m][n];
 }
